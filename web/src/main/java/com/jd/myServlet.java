81be5238c9aff6ab9864503264e9a3d5da94b39e
lmhJay <lmh1368102@126.com>
2018-03-20 17:07:23 +0800

    123

---
+++ web/src/main/java/com/jd/myServlet.java
@@ -0,0 +1,18 @@
+package com.jd;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class myServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+        Remark remark = new Remark();
+        remark.getInfo();
+    }
+}
